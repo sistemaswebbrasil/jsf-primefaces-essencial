45d78474036709695036e877603ee17708d679e8
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-10 19:24:47 -0300

    Jsf básico

---
+++ src/main/java/br/com/siswbrasil/repository/SchemaGeneration.java
@@ -0,0 +1,26 @@
+package br.com.siswbrasil.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.siswbrasil.model.Empresa;
+
+public class SchemaGeneration {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jsfPrimefacesEssencialPU");
+
+        EntityManager em = emf.createEntityManager();
+
+        List<Empresa> lista = em.createQuery("from Empresa", Empresa.class).getResultList();
+
+        System.out.println(lista);
+
+        em.close();
+        emf.close();
+    }
+
+}
--- src/main/java/br/com/siswbrasil/util/EntityManagerProducer.java
+++ src/main/java/br/com/siswbrasil/util/EntityManagerProducer.java
@@ -14,7 +14,7 @@ public class EntityManagerProducer {
 	private EntityManagerFactory factory;
 	
 	public EntityManagerProducer() {
-		this.factory = Persistence.createEntityManagerFactory("AlgaWorksPU");
+		this.factory = Persistence.createEntityManagerFactory("jsfPrimefacesEssencialPU");
 	}
 	
 	@Produces
