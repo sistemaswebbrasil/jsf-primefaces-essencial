6c349d30906572b1a94c1cb5992acea906a0cb27
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-11 14:05:45 -0300

    Autocomplete

--- src/main/java/br/com/siswbrasil/bean/GestaoEmpresasBean.java
+++ src/main/java/br/com/siswbrasil/bean/GestaoEmpresasBean.java
@@ -3,45 +3,63 @@ package br.com.siswbrasil.bean;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import br.com.siswbrasil.converter.RamoAtividadeConverter;
 import br.com.siswbrasil.model.Empresa;
+import br.com.siswbrasil.model.RamoAtividade;
+import br.com.siswbrasil.model.TipoEmpresa;
 import br.com.siswbrasil.repository.EmpresaRepository;
+import br.com.siswbrasil.repository.RamoAtividadeRepository;
 import br.com.siswbrasil.util.FacesMessages;
 
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    
-    @Inject
-    private EmpresaRepository empresaRepository;
-    
-    @Inject
-    private FacesMessages messages;    
-    
-    private List<Empresa> listaEmpresas;
-    
-    private String termoPesquisa;
-    
-    public void pesquisar() {
-        listaEmpresas = empresaRepository.pesquisar(termoPesquisa);
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private EmpresaRepository empresaRepository;
+
+	@Inject
+	private RamoAtividadeRepository ramoAtividadeRepository;
+
+	@Inject
+	private FacesMessages messages;
+
+	private List<Empresa> listaEmpresas;
+
+	private String termoPesquisa;
+	
+	private Converter ramoAtividadeConverter;
+
+	public void pesquisar() {
+		listaEmpresas = empresaRepository.pesquisar(termoPesquisa);
+
+		if (listaEmpresas.isEmpty()) {
+			messages.info("Sua consulta não retornou registros.");
+		}
+	}
+
+	public void todasEmpresas() {
+		listaEmpresas = empresaRepository.todas();
+	}
+	
+    public List<RamoAtividade> completarRamoAtividade(String termo) {
+        List<RamoAtividade> listaRamoAtividades = ramoAtividadeRepository.pesquisar(termo);
         
-        if (listaEmpresas.isEmpty()) {
-            messages.info("Sua consulta não retornou registros.");
-        }
-    }    
-    
-    public void todasEmpresas() {
-        listaEmpresas = empresaRepository.todas();
-    }
-    
-    public List<Empresa> getListaEmpresas() {
-        return listaEmpresas;
-    }
+        ramoAtividadeConverter = new RamoAtividadeConverter(listaRamoAtividades);
+        
+        return listaRamoAtividades;
+    }	
+
+	public List<Empresa> getListaEmpresas() {
+		return listaEmpresas;
+	}
 
 	public String getTermoPesquisa() {
 		return termoPesquisa;
@@ -50,7 +68,15 @@ public class GestaoEmpresasBean implements Serializable {
 	public void setTermoPesquisa(String termoPesquisa) {
 		this.termoPesquisa = termoPesquisa;
 	}
-    
-    
+
+	public TipoEmpresa[] getTiposEmpresa() {
+		return TipoEmpresa.values();
+	}
+
+	public Converter getRamoAtividadeConverter() {
+		return ramoAtividadeConverter;
+	}
+	
+	
 
 }
