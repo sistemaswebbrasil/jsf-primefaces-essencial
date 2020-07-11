edef72d825c7cca5ee62a8c917d8d02a31ac34d6
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-11 11:59:12 -0300

    Mostrando empresas

--- src/main/java/br/com/siswbrasil/bean/GestaoEmpresasBean.java
+++ src/main/java/br/com/siswbrasil/bean/GestaoEmpresasBean.java
@@ -1,12 +1,14 @@
 package br.com.siswbrasil.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.com.siswbrasil.model.Empresa;
-import br.com.siswbrasil.model.TipoEmpresa;
+import br.com.siswbrasil.repository.EmpresaRepository;
 
 @Named
 @ViewScoped
@@ -14,24 +16,17 @@ public class GestaoEmpresasBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     
-    private Empresa empresa ;
+    @Inject
+    private EmpresaRepository empresaRepository;
     
-    public void salvar() {
-        System.out.println("Razão social: " + empresa.getRazaoSocial()
-                + " - Nome fantasia: " + empresa.getNomeFantasia()
-                + " - Tipo: " + empresa.getTipo());
+    private List<Empresa> listaEmpresas;
+    
+    public void todasEmpresas() {
+        listaEmpresas = empresaRepository.todas();
     }
     
-    public String ajuda() {
-        return "AjudaGestaoEmpresas?faces-redirect=true";
+    public List<Empresa> getListaEmpresas() {
+        return listaEmpresas;
     }
-    
-    
-    public Empresa getEmpresa() {
-        return empresa;
-    }
-    
-    public TipoEmpresa[] getTiposEmpresa() {
-        return TipoEmpresa.values();
-    }
+
 }
--- src/main/java/br/com/siswbrasil/repository/EmpresaRepository.java
+++ src/main/java/br/com/siswbrasil/repository/EmpresaRepository.java
@@ -37,6 +37,10 @@ public class EmpresaRepository implements Serializable {
 		return query.getResultList();
 	}
 
+	public List<Empresa> todas() {
+		return manager.createQuery("from Empresa", Empresa.class).getResultList();
+	}
+
 	public Empresa guardar(Empresa empresa) {
 		return manager.merge(empresa);
 	}
